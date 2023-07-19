696bc8362b9501996c04734ae36d39068e0fda8a
quangduy201 <dinhduy2012003@gmail.com>
2023-07-19 13:53:46 +0700

    BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Portfolio from "./Portfolio/Portfolio";
 import PrivacyPolicy from "./PrivacyPolicy/PrivacyPolicy";
